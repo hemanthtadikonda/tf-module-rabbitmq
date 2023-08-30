11fef698fc90c131b32b3886021392ce1aa47b0a
hemanth <hemanthtadikonda29@gmail.com>
2023-08-30 14:47:58 +0530

    up to - apps setup completed

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
 
   name_prefix = "${var.env}-rabbitmq"
-  tags        = merge ( var.tags , { tf-module = rabbitmq } , {env = var.env} )
+  tags        = merge ( var.tags , { tf-module = "rabbitmq" } , {env = var.env} )
 }
